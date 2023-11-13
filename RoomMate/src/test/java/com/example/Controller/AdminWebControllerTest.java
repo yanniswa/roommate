938133c87fe4b39ca91f88987480689adab47573
Yannis Wagner <yanniswagner@web.de>
2023-11-13 14:16:58 +0100

    Admin Controller gibt für Get Requests auf /admin HTML Seite mit Funktionen zurück

---
+++ RoomMate/src/test/java/com/example/Controller/AdminWebControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.Controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+@WebMvcTest(AdminWebController.class)
+public class AdminWebControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Get Request auf /admin funktioniert")
+    void test_1() throws Exception {
+        mockMvc.perform(get("/admin"))
+                .andExpect(status().isOk());
+    }
+
+}
