3f2ee392da32996094161f599bb2e9d6e861b53d
Yannis Wagner <yanniswagner@web.de>
2023-12-29 20:52:02 +0100

    URL auf Raumnummer hinzugefügt

--- RoomMate/src/main/java/com/example/Controller/UserWebController.java
+++ RoomMate/src/main/java/com/example/Controller/UserWebController.java
@@ -3,11 +3,15 @@ package com.example.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UserWebController {
 
-
+    @GetMapping("/user/{roomNumber}")
+    public String plaetzeUebersicht(@PathVariable Integer roomNumber){
+        return "index";
+    }
     @GetMapping("/")
     public String index(Model model, RoomService roomService){
         model.addAttribute("raumList",roomService.getAllRooms());
--- RoomMate/src/test/java/com/example/Controller/UserWebControllerTests.java
+++ RoomMate/src/test/java/com/example/Controller/UserWebControllerTests.java
@@ -9,6 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest
 public class UserWebControllerTests {
@@ -17,10 +18,21 @@ public class UserWebControllerTests {
     MockMvc mvc;
 
     @Test
-    @DisplayName("Get Request auf / funktioniert")
+    @DisplayName("Get Request auf /user funktioniert")
     @WithMockOAuth2User()
     void test_01() throws Exception{
-        mvc.perform(get("/"))
+        mvc.perform(get("/user"))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    @DisplayName("Get Request gibt richtige HTML Datei zuück")
+    @WithMockOAuth2User()
+    void test_02() throws Exception {
+        mvc.perform(get("/user"))
+                .andExpect(view().name("user"));
+    }
+
+
+
 }
