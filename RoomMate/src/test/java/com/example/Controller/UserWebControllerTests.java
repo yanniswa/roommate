84bdc57d80549559137ad99f01898307f8b65118
Yannis Wagner <yanniswagner@web.de>
2024-01-06 19:53:05 +0100

    UserWebControllerTests gefixxt

--- RoomMate/src/test/java/com/example/Controller/UserWebControllerTests.java
+++ RoomMate/src/test/java/com/example/Controller/UserWebControllerTests.java
@@ -1,10 +1,14 @@
 package com.example.Controller;
 
 import com.example.Controller.Helper.WithMockOAuth2User;
+import com.example.Controller.domain.applicationservice.BuchungsService;
+import com.example.Controller.domain.applicationservice.RoomService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -16,6 +20,10 @@ public class UserWebControllerTests {
 
     @Autowired
     MockMvc mvc;
+    @MockBean
+    RoomService roomService;
+    @MockBean
+    BuchungsService buchungsService;
 
     @Test
     @DisplayName("Get Request auf /user funktioniert")
