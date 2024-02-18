a5322d4bb9cec4478e9eb7f9b3f4242175cd2311
kruegertobias <tobiasvkrueger@gmail.com>
2024-02-18 12:18:58 +0100

    Html und Css in Admin und User erweitert

--- RoomMate/src/main/java/RoomMate/config/SecurityConfig.java
+++ RoomMate/src/main/java/RoomMate/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
         @Bean
         public SecurityFilterChain configure(HttpSecurity chainBuilder) throws Exception {
             chainBuilder.authorizeRequests(a-> a
-                            .requestMatchers("/css/**").permitAll()
+                            .requestMatchers("/css/*").permitAll()
                             .anyRequest().authenticated()
                     )
                     .logout(c->c.logoutSuccessUrl("/"))
