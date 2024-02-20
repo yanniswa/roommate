7bfa21c22a78f78815ae4b3f80ee781948fceef9
kruegertobias <tobiasvkrueger@gmail.com>
2024-02-20 13:40:18 +0100

    Arbeitsplätze können hinzugefügt werden

--- RoomMate/src/main/java/RoomMate/service/AdminService.java
+++ RoomMate/src/main/java/RoomMate/service/AdminService.java
@@ -3,10 +3,13 @@ package RoomMate.service;
 import RoomMate.database.ArbeitsplatzRepositoryImpl;
 import RoomMate.domain.model.Arbeitsplatz;
 import RoomMate.domain.model.Buchung;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+@Service
 public class AdminService {
     private final ArbeitsplatzRepository repository;
 
@@ -14,4 +17,9 @@ public class AdminService {
         this.repository = repository;
     }
 
+    public Arbeitsplatz addArbeitsplatz(int raumnummer, Set<String> ausstattung) {
+        Arbeitsplatz arbeitsplatz = new Arbeitsplatz(raumnummer, ausstattung);
+        Arbeitsplatz gespeicherterArbeitsplatz = repository.save(arbeitsplatz);
+        return gespeicherterArbeitsplatz;
+    }
 }
--- RoomMate/src/main/java/RoomMate/web/AdminWebController.java
+++ RoomMate/src/main/java/RoomMate/web/AdminWebController.java
@@ -1,11 +1,17 @@
 package RoomMate.web;
 
+import RoomMate.domain.model.Arbeitsplatz;
+import RoomMate.service.AdminService;
 import RoomMate.service.BuchungsService;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/admin")
@@ -14,8 +20,11 @@ public class AdminWebController {
 
     private final BuchungsService buchungsService;
 
-    public AdminWebController(BuchungsService buchungsService) {
+    private final AdminService adminService;
+
+    public AdminWebController(BuchungsService buchungsService, AdminService adminService) {
         this.buchungsService = buchungsService;
+        this.adminService = adminService;
     }
 
     @GetMapping()
@@ -23,5 +32,12 @@ public class AdminWebController {
         model.addAttribute("arbeitsplaetze",buchungsService.alleArbeitsplaetze());
         model.addAttribute("alleBuchungen",buchungsService.alleBuchungen());
         return "adminFunktionen";
+    }
+
+    @PostMapping()
+    public String platzhinzufuegen(int raumnummer, String ausstattung1){
+        Set<String> ausstattung = Set.of(ausstattung1);
+        adminService.addArbeitsplatz(raumnummer, ausstattung);
+        return "redirect:/admin";
     }
 }
